db23551f3aaddf09b561f3885e2625d4a1541b3d
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-08-29 22:40:29 -0700

    refactor useData hook to generalize

--- app/(routes)/dashboard/FlashCard.tsx
+++ app/(routes)/dashboard/FlashCard.tsx
@@ -2,37 +2,13 @@
 import { useEffect, useState } from "react";
 import Card from "../../_components/Card";
 import CardNavigation from "../../_components/CardNavigation";
-import useData from "@/app/_hooks/useData";
-import Loading from "@/app/_components/Loading";
-import CardContainer from "@/app/_components/CardContainer";
 import CardSkeleton from "@/app/_components/CardSkeleton";
-
-export interface Quiz {
-  question: string;
-  answers: {
-    answer_a: string | null;
-    answer_b: string | null;
-    answer_c: string | null;
-    answer_d: string | null;
-    answer_e: string | null;
-    answer_f: string | null;
-  };
-  correct_answers: {
-    answer_a_correct: string | null;
-    answer_b_correct: string | null;
-    answer_c_correct: string | null;
-    answer_d_correct: string | null;
-    answer_e_correct: string | null;
-    answer_f_correct: string | null;
-  };
-}
+import CardContainer from "@/app/_components/CardContainer";
+import Loading from "@/app/_components/Loading";
+import useQuiz, { Quiz } from "@/app/_hooks/useQuiz";
 
 export default function FlashCard() {
-  const BASE_URL = "https://quizapi.io/api/v1/questions";
-  const limit = 10;
-  const API_KEY = process.env.NEXT_PUBLIC_QUIZ_API_KEY;
-  const URL = BASE_URL + "?apiKey=" + API_KEY + "&limit=" + limit;
-  const { data, error, isLoading } = useData(URL);
+  const { data, error, isLoading } = useQuiz();
   const [currentCard, setCurrentCard] = useState<Quiz | undefined>();
 
   useEffect(() => {
@@ -53,8 +29,10 @@ export default function FlashCard() {
       )}
       {currentCard && (
         <>
-          <Card quiz={currentCard} />
-          <CardNavigation cardLimit={data ? data.length : 0} />
+          <CardContainer>
+            <Card quiz={currentCard} />
+            <CardNavigation cardLimit={data ? data.length : 0} />
+          </CardContainer>
         </>
       )}
     </>
--- app/_hooks/useData.ts
+++ app/_hooks/useData.ts
@@ -1,40 +1,42 @@
 import { useEffect, useState } from "react";
-import { Quiz } from "../(routes)/dashboard/FlashCard";
 
-const useData =  (endpoint: string) => {
-    const [data, setData] = useState<Quiz[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const controller = new AbortController();
-        const signal = controller.signal;
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
 
-        setLoading(true);
+    setLoading(true);
 
-        fetch(endpoint, { signal })
-            .then((res) => {
-                // Check if the response status is in the 200 range (success status)
-                if (res.ok) {
-                    // res.json stills gives us a promise, so we need to chain another then()
-                    return res.json();
-                } else {
-                    // Reject the promise with the status code
-                    throw new Error(`HTTP error, Status: ${res.status}`);
-                }
-            })
-            .then((result) => {
-                setData(result);
-                setLoading(false);
-            })
-            .catch((error) => {
-                const message = "There was an error fetching data: " + error;
-                setError(message);
-                setLoading(false);
-            });
-        }, [])
-        
-        return {data, error, isLoading};
-}
+    fetch(endpoint, { signal })
+      .then((res) => {
+        // Check if the response status is in the 200 range (success status)
+        if (res.ok) {
+          // res.json stills gives us a promise, so we need to chain another then()
+          return res.json();
+        } else {
+          // Reject the promise with the status code
+          throw new Error(`HTTP error, Status: ${res.status}`);
+        }
+      })
+      .then((result) => {
+        setData(result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        const message = "There was an error fetching data: " + error;
+        setError(message);
+        setLoading(false);
+        return;
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { data, error, isLoading };
+};
 
 export default useData;
